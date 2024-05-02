e4ab14426d0848592d73d08aab76c97c75a7d797
alansomathew <alansomathew10@gmail.com>
2024-05-02 11:16:01 +0530

    changes

--- lib/features/expense/screens/graph.dart
+++ lib/features/expense/screens/graph.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:walletview/common/widget/appbar/appbar.dart';
 import 'package:walletview/common/widget/appbar/drawer.dart';
 import 'package:walletview/features/expense/screens/widgets/piechart_graph.dart';
+import 'package:walletview/utils/constants/size.dart';
 
 class GraphScreen extends StatelessWidget {
   const GraphScreen({super.key});
@@ -14,6 +15,14 @@ class GraphScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            SizedBox(
+              width: double.infinity, // Example width
+              height: 400, // Example height
+              child: PieChartGraph(),
+            ),
+            SizedBox(
+              height: WSizes.defaultSpace/2,
+            ),
             SizedBox(
               width: double.infinity, // Example width
               height: 400, // Example height
